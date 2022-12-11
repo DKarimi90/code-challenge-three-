e46edffecb5f0fafad72fe617426dfe82762d0ef
david <davmwa90@gmail.com>
2022-12-11 15:28:49 +0300

    added content to index.js file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 document.getElementById("btn"). addEventListener("click", getFilms)
+document.getElementById("btn2"). addEventListener("click", buyTicket)
 
 function getFilms() {
-    fetch("./db.json")
+    let baseUrl = "./db.json"
+    fetch(baseUrl)
     .then(res => res.json())
     .then(data => {
         let output = "<h2>Films</h2>"
@@ -22,4 +24,25 @@ function getFilms() {
 
 }
 
+function buyTicket() {
+    let baseUrl = "./db.json"
+    fetch(baseUrl)
+    .then(res => res.json())
+    .then(data => {
+        let output = "<h3>Available Tickets</h3>"
+        const array = data.films
+        array.forEach(function(films){
+            output += 
+            `<div>
+                <h6>${films.title}</h6>
+                <h4> Tickets: ${films.capacity - films.tickets_sold}</h4>
+            </div>
+            `
+        })
+        document.getElementById('output').innerHTML = output; 
+    })
+
+}
+
+    
 
